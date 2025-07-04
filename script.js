c5b10f92d618df5a4bc9977107d4b2e30abd67c6
NathaliaSueckk <164500481+NathaliaSueckk@users.noreply.github.com>
2025-07-04 08:53:30 -0300

    g

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
@@ -56,8 +57,16 @@ function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
     const botaoAlternativas = document.createElement("button");
     botaoAlternativas.textContent = alternativa;
+    botao.addEventListener("click", () => respostaSelecionada(opcao));
     caixaAlternativas.appendChild(botaoAlternativas);
   }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+  const afirmacoes = opcaoSelecionada.afirmacoes;
+  historiaFinal += afirmacoes + " ";
+  atual++;
+  mostraPergunta();
 }
 
 mostraPergunta();
